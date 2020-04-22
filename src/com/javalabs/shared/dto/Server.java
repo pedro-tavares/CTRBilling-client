a7879ef811e7d021b26c8588c0000689db61d433
Pedro Alexandre Tavares <patavares@outlook.com>
2020-04-22 20:45:04 +0200

    Server DTO

--- src/com/javalabs/shared/dto/Server.java
+++ src/com/javalabs/shared/dto/Server.java
@@ -6,16 +6,58 @@ public class Server implements Serializable {
 
 	private static final long serialVersionUID = 2L;
 	
+	private String name;
 	private String ipAddress;
 	private int port;
 	private String username;
 	private String password;
 	
-	public void Server(String ipAddress, int port, String username, String password) {
+	public Server(String name, String ipAddress, int port, String username, String password) {
+		this.name = name;
 		this.ipAddress = ipAddress;
 		this.port = port;
 		this.username = username;
 		this.password = password;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
